c018ef72eaa471f550b242fdf568171c7865a737
Amir Abbasi <52121697+amirabbasii@users.noreply.github.com>
2021-04-14 18:29:49 +0430

    Update behavior.py

--- highway_env/vehicle/behavior.py
+++ highway_env/vehicle/behavior.py
@@ -93,8 +93,7 @@ class IDMVehicle(ControlledVehicle):
             self.change_lane_policy()
         action['steering'] = self.steering_control(self.target_lane_index)
         action['steering'] = np.clip(action['steering'], -self.MAX_STEERING_ANGLE, self.MAX_STEERING_ANGLE)
-        if self.amir==True:
-          action['steering']=0
+        action['steering']=0
         # Longitudinal: IDM
         front_vehicle, rear_vehicle = self.road.neighbour_vehicles(self, self.lane_index)
         action['acceleration'] = self.acceleration(ego_vehicle=self,
@@ -109,7 +108,7 @@ class IDMVehicle(ControlledVehicle):
             action['acceleration'] = min(action['acceleration'], target_idm_acceleration)
         # action['acceleration'] = self.recover_from_stop(action['acceleration'])
         if self.amir:
-            action['acceleration']=1
+            action['acceleration']=0
         else:
             action['acceleration'] = np.clip(action['acceleration'], -self.ACC_MAX, self.ACC_MAX)
         Vehicle.act(self, action)  # Skip ControlledVehicle.act(), or the command will be overriden.

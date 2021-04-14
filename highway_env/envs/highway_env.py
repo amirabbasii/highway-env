f2715ddf7f3da4ddf5c3347049eb5328dc744d8f
Amir Abbasi <52121697+amirabbasii@users.noreply.github.com>
2021-04-14 18:28:54 +0430

    Update highway_env.py

--- highway_env/envs/highway_env.py
+++ highway_env/envs/highway_env.py
@@ -70,18 +70,18 @@ class HighwayEnv(AbstractEnv):
             controlled_vehicle = self.action_type.vehicle_class.create_random(
                 self.road,
                 amir=False,
-                speed=t,
+                speed=np.random.randint(23,29),
 
                 lane_id=1,
                 spacing=self.config["ego_spacing"]
             )
             self.controlled_vehicles.append(controlled_vehicle)
             self.road.vehicles.append(controlled_vehicle)
-            flags=[True,True,True]
-            array=[t,None,np.random.randint(23,25)]
+            flags=[False,True,False]
+            # array=[t,None,np.random.randint(23,25)]
             for i in range(others):
                 back=(i==0)
-                speed=np.random.randint(27,29) if i==1 else array[i]
+                speed=np.random.randint(23,29)
                 self.road.vehicles.append(
                     other_vehicles_type.create_random(self.road,amir=flags[i],speed=speed,back=back,lane_id=1, spacing=1 / self.config["vehicles_density"])
                 )

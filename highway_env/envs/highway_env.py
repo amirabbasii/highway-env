131ebe258d99eb6d1004bdcc65558044ff24619a
Amir Abbasi <52121697+amirabbasii@users.noreply.github.com>
2021-04-16 18:32:10 +0430

    Update highway_env.py

--- highway_env/envs/highway_env.py
+++ highway_env/envs/highway_env.py
@@ -67,27 +67,35 @@ class HighwayEnv(AbstractEnv):
         other_per_controlled = near_split(self.config["vehicles_count"], num_bins=self.config["controlled_vehicles"])
 
         self.controlled_vehicles = []
-        t=np.random.randint(23,25)
+        t=np.random.randint(11)
         for others in other_per_controlled:
-            controlled_vehicle = self.action_type.vehicle_class.create_random(
-                self.road,
-                amir=False,
-                speed=np.random.randint(23,29),
+            # controlled_vehicle = self.action_type.vehicle_class.create_random(
+            #     self.road,
+            #     amir=False,
+            #     speed=np.random.randint(23,29),
 
-                lane_id=1,
-                spacing=self.config["ego_spacing"]
-            )
-            self.controlled_vehicles.append(controlled_vehicle)
-            self.road.vehicles.append(controlled_vehicle)
-            flags=[False,True,False]
+            #     lane_id=1,
+            #     spacing=self.config["ego_spacing"]
+            # )
+            flags=[False,False]
             # array=[t,None,np.random.randint(23,25)]
-            for i in range(others):
-                back=(i==0)
-                speed=np.random.randint(23,29)
-                self.road.vehicles.append(
-                    other_vehicles_type.create_random(self.road,amir=flags[i],speed=speed,back=back,lane_id=1, spacing=1 / self.config["vehicles_density"])
-                )
-                # t=(t+1)%3
+            for i in range(others+1):
+                if i==t:
+                    controlled_vehicle=self.action_type.vehicle_class.create_random(
+                    self.road,
+                    amir=False,
+                    speed=np.random.randint(23,29),
+
+                    lane_id=1,
+                    spacing=self.config["ego_spacing"])
+                    self.controlled_vehicles.append(controlled_vehicle)
+                    self.road.vehicles.append(controlled_vehicle)
+                else:
+                  back=False
+                  speed=np.random.randint(23,29)
+                  self.road.vehicles.append(
+                      other_vehicles_type.create_random(self.road,amir=False,speed=speed,back=back, spacing=1 / self.config["vehicles_density"])
+                  )
 
     def _reward(self, action: Action) -> float:
         """

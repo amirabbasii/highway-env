09d860fd929bcd692391caaa611ed89bbc292063
Amir Abbasi <52121697+amirabbasii@users.noreply.github.com>
2021-04-17 02:11:47 +0430

    Update kinematics.py

--- highway_env/vehicle/kinematics.py
+++ highway_env/vehicle/kinematics.py
@@ -1,256 +1,131 @@
-from typing import Union, Optional
 import numpy as np
-from collections import deque
+from typing import Tuple
+from gym.envs.registration import register
 
 from highway_env import utils
-from highway_env.road.road import Road, LaneIndex
-from highway_env.vehicle.objects import RoadObject, Obstacle, Landmark
-from highway_env.types import Vector
+from highway_env.envs.common.abstract import AbstractEnv
+from highway_env.envs.common.action import Action
+from highway_env.road.road import Road, RoadNetwork
+from highway_env.utils import near_split
+from highway_env.vehicle.controller import ControlledVehicle
 
 
-class Vehicle(RoadObject):
-
+class HighwayEnv(AbstractEnv):
     """
-    A moving vehicle on a road, and its kinematics.
+    A highway driving environment.
 
-    The vehicle is represented by a dynamical system: a modified bicycle model.
-    It's state is propagated depending on its steering and acceleration actions.
+    The vehicle is driving on a straight highway with several lanes, and is rewarded for reaching a high speed,
+    staying on the rightmost lanes and avoiding collisions.
     """
 
-    COLLISIONS_ENABLED = True
-    """ Enable collision detection between vehicles """
+    RIGHT_LANE_REWARD: float = 0.1
+    """The reward received when driving on the right-most lanes, linearly mapped to zero for other lanes."""
 
-    LENGTH = 5.0
-    """ Vehicle length [m] """
-    WIDTH = 2.0
-    """ Vehicle width [m] """
-    DEFAULT_SPEEDS = [23, 25]
-    """ Range for random initial speeds [m/s] """
-    MAX_SPEED = 40.
-    """ Maximum reachable speed [m/s] """
+    HIGH_SPEED_REWARD: float = 0.4
+    """The reward received when driving at full speed, linearly mapped to zero for lower speeds according to config["reward_speed_range"]."""
 
-    def __init__(self,
-                 road: Road,
-                 position: Vector,
-                 heading: float = 0,
-                 speed: float = 0,
-                 amir:bool=False,
-                ):
-        super().__init__(road, position, heading, speed)
-        self.action = {'steering': 0, 'acceleration': 0}
-        self.crashed = False
-        self.impact = None
-        self.log = []
-        self.history = deque(maxlen=30)
+    LANE_CHANGE_REWARD: float = 0
+    """The reward received at each lane change action."""
+    
+
+    @classmethod
+    def default_config(cls) -> dict:
+        config = super().default_config()
+        config.update({
+            "observation": {
+                "type": "Kinematics"
+            },
+            "action": {
+                "type": "DiscreteMetaAction",
+            },
+            "lanes_count": 3,
+            "vehicles_count": 2,
+            "controlled_vehicles": 1,
+            "initial_lane_id": None,
+            "duration": 20,  # [s]
+            "ego_spacing": 2,
+            "vehicles_density": 1,
+            "collision_reward": -1,  # The reward received when colliding with a vehicle.
+            "reward_speed_range": [0, 30],
+            "offroad_terminal": False
+        })
+        return config
+
+    def _reset(self) -> None:
+        self._create_road()
+        self._create_vehicles()
         self.alarm=False
 
-    @classmethod
-    def make_on_lane(cls, road: Road, lane_index: LaneIndex, longitudinal: float, speed: float = 0) -> "Vehicle":
+    def _create_road(self) -> None:
+        """Create a road composed of straight adjacent lanes."""
+        self.road = Road(network=RoadNetwork.straight_road_network(self.config["lanes_count"]),
+                         np_random=self.np_random, record_history=self.config["show_trajectories"])
+
+    def _create_vehicles(self) -> None:
+        """Create some new random vehicles of a given type, and add them on the road."""
+        other_vehicles_type = utils.class_from_path(self.config["other_vehicles_type"])
+        other_per_controlled = near_split(self.config["vehicles_count"], num_bins=self.config["controlled_vehicles"])
+
+        self.controlled_vehicles = []
+        t=0
+        l=np.random.randint(3)
+        for others in other_per_controlled:
+            # controlled_vehicle = self.action_type.vehicle_class.create_random(
+            #     self.road,
+            #     amir=False,
+            #     speed=np.random.randint(23,29),
+
+            #     lane_id=1,
+            #     spacing=self.config["ego_spacing"]
+            # )
+            flags=[False,True,False]
+            # array=[t,None,np.random.randint(23,25)]
+            for i in range(others+1):
+                if i==t:
+                    controlled_vehicle=self.action_type.vehicle_class.create_random(
+                    self.road,
+                    amir=False,
+                    
+                    speed=np.random.randint(23,29),
+
+                    lane_id=1,
+                    spacing=self.config["ego_spacing"])
+                    self.controlled_vehicles.append(controlled_vehicle)
+                    self.road.vehicles.append(controlled_vehicle)
+                else:
+                  back=False
+                  speed=np.random.randint(23,29)
+                  self.road.vehicles.append(
+                      other_vehicles_type.create_random(self.road,amir=flags[i],lane_id=l,speed=speed,back=back, spacing=1 / self.config["vehicles_density"],ego=True)
+                  )
+
+    def _reward(self, action: Action) -> float:
         """
-        Create a vehicle on a given lane at a longitudinal position.
-
-        :param road: the road where the vehicle is driving
-        :param lane_index: index of the lane where the vehicle is located
-        :param longitudinal: longitudinal position along the lane
-        :param speed: initial speed in [m/s]
-        :return: A vehicle with at the specified position
+        The reward is defined to foster driving at high speed, on the rightmost lanes, and to avoid collisions.
+        :param action: the last action performed
+        :return: the corresponding reward
         """
-        lane = road.network.get_lane(lane_index)
-        if speed is None:
-            speed = lane.speed_limit
-        return cls(road, lane.position(longitudinal, 0), lane.heading_at(longitudinal), speed)
+        reward= utils.lmap(self.vehicle.speed, self.config["reward_speed_range"], [0, 1])
+        if self.vehicle.crashed:
+          reward=self.config["collision_reward"] * reward
+        
+        return reward
 
-    @classmethod
-    def create_random(cls, road: Road,
-                      speed: float = None,
-                      amir:Optional[bool]=False,
-                      back:Optional[bool]=False,
-                      lane_from: Optional[str] = None,
-                      lane_to: Optional[str] = None,
-                      lane_id: Optional[int] = None,
-                      spacing: float = 1) \
-            -> "Vehicle":
-        """
-        Create a random vehicle on the road.
 
-        The lane and /or speed are chosen randomly, while longitudinal position is chosen behind the last
-        vehicle in the road with density based on the number of lanes.
+    def _is_terminal(self) -> bool:
+        """The episode is over if the ego vehicle crashed or the time is out."""
+        
+        return self.vehicle.crashed or \
+            self.steps >= self.config["duration"] or \
+            self.vehicle.speed<=0 or \
+            (self.config["offroad_terminal"] and not self.vehicle.on_road)
 
-        :param road: the road where the vehicle is driving
-        :param speed: initial speed in [m/s]. If None, will be chosen randomly
-        :param lane_from: start node of the lane to spawn in
-        :param lane_to: end node of the lane to spawn in
-        :param lane_id: id of the lane to spawn in
-        :param spacing: ratio of spacing to the front vehicle, 1 being the default
-        :return: A vehicle with random position and/or speed
-        """
-        _from = lane_from or road.np_random.choice(list(road.network.graph.keys()))
-        _to = lane_to or road.np_random.choice(list(road.network.graph[_from].keys()))
-        _id = lane_id if lane_id is not None else road.np_random.choice(len(road.network.graph[_from][_to]))
-        lane = road.network.get_lane((_from, _to, _id))
-        if speed is None:
-            if lane.speed_limit is not None:
-                speed = road.np_random.uniform(0.7*lane.speed_limit, lane.speed_limit)
-            else:
-                speed = road.np_random.uniform(Vehicle.DEFAULT_SPEEDS[0], Vehicle.DEFAULT_SPEEDS[1])
-        default_spacing = np.random.randint(10,20)
-        offset = spacing * default_spacing * np.exp(-5 / 40 * len(road.network.graph[_from][_to]))
-        x0 = np.max([lane.local_coordinates(v.position)[0] for v in road.vehicles]) \
-            if len(road.vehicles) else 3*offset
-        x0 += offset * road.np_random.uniform(0.9, 1.1)
-        if back:
-          x0=road.vehicles[0].position[0]-10
-        v = cls(road, lane.position(x0, 0), lane.heading_at(x0), speed,amir)
-        return v
+    def _cost(self, action: int) -> float:
+        """The cost signal is the occurrence of collision."""
+        return float(self.vehicle.crashed)
 
-    @classmethod
-    def create_from(cls, vehicle: "Vehicle") -> "Vehicle":
-        """
-        Create a new vehicle from an existing one.
 
-        Only the vehicle dynamics are copied, other properties are default.
-
-        :param vehicle: a vehicle
-        :return: a new vehicle at the same dynamical state
-        """
-        v = cls(vehicle.road, vehicle.position, vehicle.heading, vehicle.speed)
-        return v
-
-    def act(self, action: Union[dict, str] = None) -> None:
-        """
-        Store an action to be repeated.
-
-        :param action: the input action
-        """
-        if action:
-            self.action = action
-
-    def step(self, dt: float) -> None:
-
-        """
-        Propagate the vehicle state given its actions.
-
-        Integrate a modified bicycle model with a 1st-order response on the steering wheel dynamics.
-        If the vehicle is crashed, the actions are overridden with erratic steering and braking until complete stop.
-        The vehicle's current lane is updated.
-
-        :param dt: timestep of integration of the model [s]
-        """
-        self.clip_actions()
-        delta_f = self.action['steering']
-        beta = np.arctan(1 / 2 * np.tan(delta_f))
-        v = self.speed * np.array([np.cos(self.heading + beta),
-                                   np.sin(self.heading + beta)])
-        self.position += v * dt
-        if self.impact is not None:
-            self.position += self.impact
-            self.crashed = True
-            self.impact = None
-        self.heading += self.speed * np.sin(beta) / (self.LENGTH / 2) * dt
-        self.speed += self.action['acceleration'] * dt
-        self.on_state_update()
-
-    def clip_actions(self) -> None:
-        if self.crashed:
-            self.action['steering'] = 0
-#             self.action['acceleration'] = -1.0*self.speed
-        self.action['steering'] = float(self.action['steering'])
-        self.action['acceleration'] = float(self.action['acceleration'])
-        if self.speed > self.MAX_SPEED:
-            self.action['acceleration'] = min(self.action['acceleration'], 1.0 * (self.MAX_SPEED - self.speed))
-        elif self.speed < -self.MAX_SPEED:
-            self.action['acceleration'] = max(self.action['acceleration'], 1.0 * (self.MAX_SPEED - self.speed))
-
-    def on_state_update(self) -> None:
-        if self.road:
-            self.lane_index = self.road.network.get_closest_lane_index(self.position, self.heading)
-            self.lane = self.road.network.get_lane(self.lane_index)
-            if self.road.record_history:
-                self.history.appendleft(self.create_from(self))
-
-    def check_collision(self, other: 'RoadObject', dt: float = 0) -> None:
-        """
-        Check for collision with another vehicle.
-
-        :param other: the other vehicle or object
-        :param dt: timestep to check for future collisions (at constant velocity)
-        """
-        if other is self:
-            return
-
-        if isinstance(other, Vehicle):
-            if not self.COLLISIONS_ENABLED or not other.COLLISIONS_ENABLED:
-                return
-            intersecting, will_intersect, transition = self._is_colliding(other, dt)
-            if will_intersect:
-                self.impact = transition / 2
-                other.impact = -transition / 2
-            if intersecting:
-                self.crashed = other.crashed = True
-        elif isinstance(other, Obstacle):
-            if not self.COLLISIONS_ENABLED:
-                return
-            intersecting, will_intersect, transition = self._is_colliding(other, dt)
-            if will_intersect:
-                self.impact = transition
-            if intersecting:
-                self.crashed = other.hit = True
-        elif isinstance(other, Landmark):
-            intersecting, will_intersect, transition = self._is_colliding(other, dt)
-            if intersecting:
-                other.hit = True
-
-    def _is_colliding(self, other, dt):
-        # Fast spherical pre-check
-        if np.linalg.norm(other.position - self.position) > self.LENGTH + self.speed * dt:
-            return False, False, np.zeros(2,)
-        # Accurate rectangular check
-        return utils.are_polygons_intersecting(self.polygon(), other.polygon(), self.velocity * dt, other.velocity * dt)
-
-    @property
-    def velocity(self) -> np.ndarray:
-        return self.speed * self.direction  # TODO: slip angle beta should be used here
-
-    @property
-    def destination(self) -> np.ndarray:
-        if getattr(self, "route", None):
-            last_lane_index = self.route[-1]
-            last_lane_index = last_lane_index if last_lane_index[-1] is not None else (*last_lane_index[:-1], 0)
-            last_lane = self.road.network.get_lane(last_lane_index)
-            return last_lane.position(last_lane.length, 0)
-        else:
-            return self.position
-
-    @property
-    def destination_direction(self) -> np.ndarray:
-        if (self.destination != self.position).any():
-            return (self.destination - self.position) / np.linalg.norm(self.destination - self.position)
-        else:
-            return np.zeros((2,))
-
-    def to_dict(self, origin_vehicle: "Vehicle" = None, observe_intentions: bool = True) -> dict:
-        d = {
-            'presence': 1,
-            'x': self.position[0],
-            'y': self.position[1],
-            'vx': self.velocity[0],
-            'vy': self.velocity[1],
-            'heading': self.heading,
-            'cos_h': self.direction[0],
-            'sin_h': self.direction[1],
-            'cos_d': self.destination_direction[0],
-            'sin_d': self.destination_direction[1]
-        }
-        if not observe_intentions:
-            d["cos_d"] = d["sin_d"] = 0
-        if origin_vehicle:
-            origin_dict = origin_vehicle.to_dict()
-            for key in ['x', 'y', 'vx', 'vy']:
-                d[key] -= origin_dict[key]
-        return d
-
-    def __str__(self):
-        return "{} #{}: {}".format(self.__class__.__name__, id(self) % 1000, self.position)
-
-    def __repr__(self):
-        return self.__str__()
+register(
+    id='highway-v0',
+    entry_point='highway_env.envs:HighwayEnv',
+)

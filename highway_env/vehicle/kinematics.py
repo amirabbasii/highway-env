a841da6f27a0361eed878bfa108544f80a1673e3
Amir Abbasi <52121697+amirabbasii@users.noreply.github.com>
2021-04-16 18:33:39 +0430

    Update kinematics.py

--- highway_env/vehicle/kinematics.py
+++ highway_env/vehicle/kinematics.py
@@ -153,7 +153,7 @@ class Vehicle(RoadObject):
     def clip_actions(self) -> None:
         if self.crashed:
             self.action['steering'] = 0
-            self.action['acceleration'] = -1.0*self.speed
+#             self.action['acceleration'] = -1.0*self.speed
         self.action['steering'] = float(self.action['steering'])
         self.action['acceleration'] = float(self.action['acceleration'])
         if self.speed > self.MAX_SPEED:
